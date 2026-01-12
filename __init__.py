d77003c241063f28b09feb20f9822f0923fe77ba
Jan-Hendrik Müller <44469195+kolibril13@users.noreply.github.com>
2026-01-12 16:20:02 +0100

    add hello world

--- __init__.py
+++ __init__.py
@@ -1,25 +1,49 @@
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTIBILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-# General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
+import bpy
+
+class SHARENODES_PT_panel(bpy.types.Panel):
+    bl_label = "ShareNodes"
+    bl_idname = "SHARENODES_PT_panel"
+    bl_space_type = "NODE_EDITOR"
+    bl_region_type = "UI"
+    bl_category = "ShareNodes"
+
+    @classmethod
+    def poll(cls, context):
+        return context.space_data.tree_type == "GeometryNodeTree"
+
+    def draw(self, context):
+        layout = self.layout
+        layout.operator("sharenodes.export_node_tree", text="Export Node Tree")
+        layout.operator("sharenodes.import_node_tree", text="Import Node Tree")
 
 
-from . import auto_load
+class SHARENODES_OT_export(bpy.types.Operator):
+    bl_idname = "sharenodes.export_node_tree"
+    bl_label = "Export Node Tree"
 
-auto_load.init()
+    def execute(self, context):
+        print("hello world")
+        return {"FINISHED"}
+
+
+class SHARENODES_OT_import(bpy.types.Operator):
+    bl_idname = "sharenodes.import_node_tree"
+    bl_label = "Import Node Tree"
+
+    def execute(self, context):
+        print("goodbye")
+        return {"FINISHED"}
+
 
 
 def register():
-    auto_load.register()
+    bpy.utils.register_class(SHARENODES_PT_panel)
+    bpy.utils.register_class(SHARENODES_OT_export)
+    bpy.utils.register_class(SHARENODES_OT_import)
 
 
 def unregister():
-    auto_load.unregister()
+    bpy.utils.unregister_class(SHARENODES_PT_panel)
+    bpy.utils.unregister_class(SHARENODES_OT_export)
+    bpy.utils.unregister_class(SHARENODES_OT_import)
+
